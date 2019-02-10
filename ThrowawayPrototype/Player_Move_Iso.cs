0321e6a5e48e65e40f3fc0f20cf6be9ee1dfc18a
Michael Lee <mikeslee97@gmail.com>
2019-02-10 17:45:34 -0500

    Added Player movement script

---
+++ ThrowawayPrototype/Player_Move_Iso.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move_Iso : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1.0f;
+    Vector3 upDirection, rightDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        upDirection = new Vector3(0f, 0f, 1f);
+        rightDirection = Quaternion.Euler(new Vector3(0, 90, 0)) * upDirection;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey) Move();
+    }
+
+    private void Move()
+    {
+        Vector3 rightMovement = rightDirection * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        Vector3 upMovement = upDirection * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        
+        transform.forward = Vector3.Normalize(rightMovement + upMovement);  // Changes direction that Player is facing
+        transform.position += rightMovement + upMovement;                   // Changes position of the Player
+    }
+}
