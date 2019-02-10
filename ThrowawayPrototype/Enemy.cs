e6eabfa7d7c865972c6c8838c3f5dd1acb31dd55
jaspera11 <33080050+jaspera11@users.noreply.github.com>
2019-02-10 16:12:21 -0500

    Update Enemy.cs

--- ThrowawayPrototype/Enemy.cs
+++ ThrowawayPrototype/Enemy.cs
@@ -1,21 +1,26 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     private Rigidbody2D rb2D;
+    private BoxCollider2D boxCollider;
     public float moveTime = 0.1f;
-    public bool up = false;
+    private bool up = false;
+    //public Transform PointA;
+    //public Transform PointB;
     
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    IEnumerator Move (int xDir, int yDir)
+    void startMove(int xDir, int yDir)
     {
         //Store start position to move from, based on objects current transform position.
         Vector3 start = transform.position;
@@ -23,9 +28,17 @@ public class Enemy : MonoBehaviour
         // Calculate end position based on the direction parameters passed in when calling Move.
         Vector3 end = start + new Vector3(xDir, yDir);
 
+        StartCoroutine(Move(end));
+
+    }
+
+    IEnumerator Move (Vector3 end)
+    {
+        
+
         float distanceRemaining = (transform.position - end).sqrMagnitude;
 
-        while (distanceRemaining > float.Epsilon)
+        while (distanceRemaining > 0)
         {
             Vector3 newPostion = Vector3.MoveTowards(rb2D.position, end, Time.deltaTime / moveTime);
 
@@ -40,16 +53,26 @@ public class Enemy : MonoBehaviour
 
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Enter battle if player
+        if(collision.CompareTag("Player"))
+        {
+            //enter battle
+            SceneManager.LoadScene("Battle");
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (up)
         {
-            Move(2, 1);
+            startMove(2, 1);
         }
         else
         {
-            Move(-2, -1);
+            startMove(-2, -1);
         }
         up = !up;
     }
