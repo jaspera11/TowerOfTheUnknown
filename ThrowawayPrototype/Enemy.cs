835b758c837d2bc7adcb123aab1aa1cf69b9e9f9
jaspera11 <33080050+jaspera11@users.noreply.github.com>
2019-02-11 17:39:41 -0500

    Add files via upload

--- ThrowawayPrototype/Enemy.cs
+++ ThrowawayPrototype/Enemy.cs
@@ -1,82 +1,32 @@
-//Code took inspiration from Unity 2D Roguelike tutorial: https://unity3d.com/learn/tutorials/s/2d-roguelike-tutorial
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private Rigidbody2D rb2D;
-    private BoxCollider2D boxCollider;
-    public float moveTime = 0.1f;
-    private bool up = false;
-    //public Transform PointA;
-    //public Transform PointB;
-    
-
+    private Rigidbody rb;
+    private BoxCollider boxCollider;
     // Start is called before the first frame update
     void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        boxCollider.isTrigger = true;
     }
 
-    void startMove(int xDir, int yDir)
+    private void OnTriggerEnter(Collider other)
     {
-        //Store start position to move from, based on objects current transform position.
-        Vector3 start = transform.position;
-
-        // Calculate end position based on the direction parameters passed in when calling Move.
-        Vector3 end = start + new Vector3(xDir, yDir);
-
-        StartCoroutine(Move(end));
-
-    }
-
-    IEnumerator Move (Vector3 end)
-    {
-        
-
-        float distanceRemaining = (transform.position - end).sqrMagnitude;
-
-        while (distanceRemaining > 0)
+        if (other.name == "Player")
         {
-            Vector3 newPostion = Vector3.MoveTowards(rb2D.position, end, Time.deltaTime / moveTime);
-
-            rb2D.MovePosition(newPostion);
-
-            //Recalculate the remaining distance after moving.
-            distanceRemaining = (transform.position - end).sqrMagnitude;
-
-
-            yield return null;
-        }
-
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //Enter battle if player
-        if(collision.CompareTag("Player"))
-        {
-            //enter battle
-            SceneManager.LoadScene("Battle");
+            //Load Battle scene when enemy encounters player.  Index may need to be edited
+            SceneManager.LoadScene(1);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (up)
-        {
-            startMove(2, 1);
-        }
-        else
-        {
-            startMove(-2, -1);
-        }
-        up = !up;
+        
     }
-
 }
---
+++ ThrowawayPrototype/EnemyPatrol.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// © 2017 TheFlyingKeyboard and released under MIT License
+// theflyingkeyboard.net
+//Moves object between two points
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] public float moveSpeed;
+    [SerializeField] public GameObject pointA;
+    [SerializeField] public GameObject pointB;
+    [SerializeField] private Transform enemyObject;
+
+    private bool reverseMove = false;
+    private float startTime;
+    private float journeyLength;
+    private float distCovered;
+    private float fracJourney;
+
+    void Start()
+    {
+        startTime = Time.time;
+        
+        //objectToUse = transform;
+        
+        journeyLength = Vector3.Distance(pointA.transform.position, pointB.transform.position);
+    }
+
+    
+
+    void Update()
+    {
+        distCovered = (Time.time - startTime) * moveSpeed;
+        fracJourney = distCovered / journeyLength;
+
+        if (reverseMove)
+        {
+            enemyObject.position = Vector3.Lerp(pointB.transform.position, pointA.transform.position, fracJourney);
+        }
+        else
+        {
+            enemyObject.position = Vector3.Lerp(pointA.transform.position, pointB.transform.position, fracJourney);
+        }
+
+        //Change direction when the enemy reaches a point
+        if ((Vector3.Distance(enemyObject.position, pointB.transform.position) == 0.0f || Vector3.Distance(enemyObject.position, pointA.transform.position) == 0.0f))
+        {
+            if (reverseMove)
+            {
+                reverseMove = false;
+            }
+            else
+            {
+                reverseMove = true;
+            }
+            startTime = Time.time;
+        }
+    }
+}
