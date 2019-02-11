356940bc839daa9e52177ea4b0b44250611119dc
Michael Lee <mikeslee97@gmail.com>
2019-02-11 16:32:02 -0500

    Added switch activation scripts

---
+++ ThrowawayPrototype/LevelChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChange : MonoBehaviour
+{
+    //public int sceneIndex;
+    public GameObject switchObject;
+    public int requiredSwitches;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //other.name should equal the root of your Player object
+        if (other.CompareTag("Player") && GameObject.Find(switchObject.name).GetComponent<ActivateSwitch>().GetSwitchesActivated() >= requiredSwitches)
+        {
+            //The scene number to load (in File->Build Settings)
+            //SceneManager.LoadScene(sceneIndex);
+            Application.Quit();
+        }
+    }
+}
