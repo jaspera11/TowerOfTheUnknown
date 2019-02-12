0e086b624eabdeba70f8b6a111896ee3483a2eef
jaspera11 <33080050+jaspera11@users.noreply.github.com>
2019-02-11 20:17:25 -0500

    Add files via upload

---
+++ ThrowawayPrototype/Menu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] public GameObject menu;
+
+    [SerializeField] private bool menuOn = false;
+    //private Rect rectL;
+    //private GUIContent stats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //void showMenu()
+    //{
+    //    menuOn = !menuOn;
+
+    //    if(menuOn)
+    //    {
+
+    //    } else
+    //    {
+            
+    //    }
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("enter"))
+        {
+            menuOn = !menuOn;
+        }
+
+        //Show menu
+        if(menuOn)
+        {
+            menu.SetActive(true);
+        } else
+        {
+            menu.SetActive(false);
+        }
+    }
+}
