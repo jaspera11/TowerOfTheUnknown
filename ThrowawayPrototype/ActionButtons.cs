f1049c89d89d4370f7fbb7c204018af624874728
YoungManS <seanallenryoung@ufl.edu>
2019-02-11 22:27:30 -0500

    Add files via upload

---
+++ ThrowawayPrototype/ActionButtons.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionButtons : MonoBehaviour
+{
+    public GameObject skillsMenu;
+    public GameObject stateMachine;
+    private TBCombatStateM.CombatState currState;
+    private TBCombatStateM.CombatState prevState;
+    private int attack;
+    private bool usedHealthPack;
+    public void OnAttackPressed()
+    {
+        if (currState == TBCombatStateM.CombatState.PlayerSelect)
+        {
+            prevState = TBCombatStateM.CombatState.PlayerSelect;
+            currState = TBCombatStateM.CombatState.Combat;
+            attack = 0;
+        }
+
+    }
+
+    private void Start()
+    {
+        currState = stateMachine.GetComponent<TBCombatStateM>().currState;
+        prevState = stateMachine.GetComponent<TBCombatStateM>().prevState;
+        attack = stateMachine.GetComponent<TBCombatStateM>().atk;
+        usedHealthPack = false;
+        skillsMenu.SetActive(false);
+    }
+
+    public void OnSkillsPressed()
+    {
+        skillsMenu.SetActive(true);
+    }
+
+    public void OnItemPressed()
+    {
+        usedHealthPack = true;
+        GameObject.Find("StateMachine").GetComponent<TBCombatStateM>().Player.hp = 1000;
+    }
+
+    public void OnJumpKickPressed()
+    {
+        attack = 1;
+        skillsMenu.SetActive(false);
+        prevState = TBCombatStateM.CombatState.PlayerSelect;
+        currState = TBCombatStateM.CombatState.Combat;
+    }
+
+    public void OnSpinKickPressed()
+    {
+        attack = 2;
+        skillsMenu.SetActive(false);
+        prevState = TBCombatStateM.CombatState.PlayerSelect;
+        currState = TBCombatStateM.CombatState.Combat;
+    }
+}
---
+++ ThrowawayPrototype/TBCombatStateM.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TBCombatStateM : MonoBehaviour
+{
+    
+    private CombatStateInit CombatInitScript = new CombatStateInit();
+    public Text StatusWindowText;
+    public Text PlayerHPText;
+    public Text PlayerSPText;
+    public Text EnemyHPText;
+    public int atk;
+
+    public enum CombatState
+    {
+        Initial,
+        PlayerSelect,
+        Combat,
+        EnemySelect,
+        Victory
+    }
+
+    public CombatState currState;
+    public CombatState prevState;
+    public MainChar Player;
+    public EnemyChar newEnemy;
+    int rng;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StatusWindowText.text = "";
+        PlayerHPText.text = "";
+        PlayerSPText.text = "";
+        EnemyHPText.text = "";
+        currState = CombatState.Initial;
+        newEnemy = GetComponent<EnemyChar>();
+        Player = GetComponent<MainChar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //PlayerHPText.text = "/1000";
+        //Debug.Log(Player.hp);
+        Debug.Log(currState);
+        if (Player != null && PlayerHPText != null) { PlayerHPText.text = Player.hp.ToString() + "/1000"; }
+        if (Player != null && PlayerSPText != null) { PlayerSPText.text = Player.sp.ToString() + "/100"; }
+        if (newEnemy != null && EnemyHPText != null) { EnemyHPText.text = newEnemy.hp.ToString() + "/1000"; }
+        switch (currState)
+        {
+            case (CombatState.Initial):
+                //CombatInitScript.initBattle();
+                
+                currState = CombatState.PlayerSelect;
+                break;
+            case (CombatState.PlayerSelect):
+                StatusWindowText.text = "Player's Turn...";
+                break;
+            case (CombatState.Combat):
+                if (prevState == CombatState.PlayerSelect)
+                {
+                    StatusWindowText.text = "";
+                    StatusWindowText.text = "Player made their move!";
+                    if (atk == 0)
+                    {
+                        newEnemy.hp = newEnemy.hp - 100;
+                    }
+                    if (atk == 1)
+                    {
+                        newEnemy.hp = newEnemy.hp - 150;
+                    }
+                    if (atk == 2)
+                    {
+                        newEnemy.hp = newEnemy.hp - 200;
+                    }
+                    waitToCont();
+                    if (newEnemy.hp <= 0)
+                    {
+                        newEnemy.hp = 0;
+                        StatusWindowText.text = "";
+                        StatusWindowText.text = "Enemy was defeated!";
+                        currState = CombatState.Victory;
+                    }
+                    else { currState = CombatState.EnemySelect; }
+                }
+                if (prevState == CombatState.EnemySelect)
+                {
+                    rng = Random.Range(0,1);
+                    if (rng == 0)
+                    {
+                        StatusWindowText.text = "";
+                        StatusWindowText.text = "Enemy screamed!\n Player's crit chance decreased";
+                    }
+                    if (rng == 1)
+                    {
+                        StatusWindowText.text = "";
+                        StatusWindowText.text = "Enemy threw a rock!";
+                        Player.hp = Player.hp - 20;
+                    }
+                    waitToCont();
+                    currState = CombatState.PlayerSelect;
+                }
+                break;
+            case (CombatState.EnemySelect):
+                StatusWindowText.text = "";
+                StatusWindowText.text = "Enemy's Turn...";
+                waitToCont();
+                prevState = CombatState.EnemySelect;
+                currState = CombatState.Combat;
+                break;
+            case (CombatState.Victory):
+                StatusWindowText.text = "";
+                StatusWindowText.text = "Player gained 100 exp!\n Player leveled up!";
+                waitToCont();
+                //Trigger Scene Transition
+                break;
+        }
+    }
+    private void OnGUI()
+    {
+        
+    }
+    public void Init()
+    {
+        
+    }
+    public IEnumerator waitToCont()
+    {
+        while (!Input.GetMouseButtonDown(0))
+        {
+            yield return null;
+        }
+    }
+    
+}
