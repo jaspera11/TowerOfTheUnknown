818ced3820b072c9f14d8a9efea1e0c39b53a9ef
jaspera11 <33080050+jaspera11@users.noreply.github.com>
2019-03-10 22:35:46 -0400

    Add files via upload

---
+++ StartGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+
+    [SerializeField] public int loadScene = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(loadScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
