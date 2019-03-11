5509b1b21cf8c912ddc0789d4df582297dd056c5
jaspera11 <33080050+jaspera11@users.noreply.github.com>
2019-03-11 11:27:48 -0400

    Add files via upload

---
+++ LevelChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelChange : MonoBehaviour
+{
+    public int sceneIndex;
+    public bool quit = false;
+    public GameObject switchObject;
+    public int requiredSwitches;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //other.name should equal the root of your Player object
+        if (other.CompareTag("Player") && GameObject.Find(switchObject.name).GetComponent<ActivateSwitch>().GetSwitchesActivated() >= requiredSwitches)
+        {
+            //The scene number to load (in File->Build Settings)
+            
+            if(quit)
+            {
+                Application.Quit();
+            } else
+            {
+                SceneManager.LoadScene(sceneIndex);
+            }
+            
+        }
+    }
+}
