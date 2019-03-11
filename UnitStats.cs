7b43ca5b2b5b130971582b3d41bc5b361fcb248f
Michael Lee <mikeslee97@gmail.com>
2019-03-11 09:39:20 -0400

    Some battle calculations

--- UnitStats.cs
+++ UnitStats.cs
@@ -6,12 +6,48 @@ using UnityEngine;
 
 public class UnitStats : MonoBehaviour
 {
-    [SerializeField] int health;
-    [SerializeField] int stamina;
-    [SerializeField] int attack;
-    [SerializeField] int defense;
-    [SerializeField] int speed;
-    [SerializeField] int skill;
-    [SerializeField] int luck;
-    [SerializeField] int experience = 0;
+    public int level;
+    public int health;
+    public int stamina;
+    public int attack;
+    public int defense;
+    public int speed;
+    public int skill;
+    public int luck;
+    public int experience;
+    private int maxExperience;
+    private int maxLevel;
+
+    private void Start()
+    {
+        level = 1;
+        health = 200;
+        stamina = 50;
+        attack = 10;
+        defense = 10;
+        speed = 10;
+        skill = 10;
+        luck = 10;
+        experience = 0;
+        maxExperience = 100;
+        maxLevel = 10;
+    }
+
+    private void levelUp()
+    {
+        if (level == maxLevel) return;
+        if (experience >= maxExperience)
+        {
+            experience -= maxExperience;
+            maxExperience *= 2;
+            level++;
+            health += 30;
+            stamina += 15;
+            attack += 10;
+            defense += 10;
+            speed += 10;
+            skill += 5;
+            luck += 5;
+        }
+    }
 }
