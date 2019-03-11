6cca113a9f65824025531c4a9ac3c00bdba27ec1
Michael Lee <mikeslee97@gmail.com>
2019-03-11 08:04:52 -0400

    Scripts for persistent data

---
+++ UnitStats.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attach to whatever unit you want
+
+public class UnitStats : MonoBehaviour
+{
+    [SerializeField] int health;
+    [SerializeField] int stamina;
+    [SerializeField] int attack;
+    [SerializeField] int defense;
+    [SerializeField] int speed;
+    [SerializeField] int skill;
+    [SerializeField] int luck;
+    [SerializeField] int experience = 0;
+}
