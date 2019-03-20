0ba1500d57019ab50eabe7f2ae4514d6b3a30906
jaspera11 <33080050+jaspera11@users.noreply.github.com>
2019-03-20 15:08:35 -0400

    Add files via upload

---
+++ BTAI/If.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace BT
+{
+    public class If : Node
+    {
+        private Func<bool> m_f;
+        protected List<Node> m_nodes = new List<Node>();
+
+        public If(Func<bool> f, List<Node> nodes)
+        {
+            m_f = f;
+            m_nodes = nodes;
+        }
+
+        public override NodeStates Evaluate()
+        {
+            if (m_f())
+            {
+                foreach (Node node in m_nodes)
+                {
+                    switch (node.Evaluate())
+                    {
+                        case NodeStates.FAILURE:
+                            continue;
+                        case NodeStates.SUCCESS:
+                            m_nodeState = NodeStates.SUCCESS;
+                            return m_nodeState;
+                        case NodeStates.RUNNING:
+                            m_nodeState = NodeStates.RUNNING;
+                            return m_nodeState;
+                        default:
+                            continue;
+                    }
+                }
+                m_nodeState = NodeStates.FAILURE;
+                return m_nodeState;
+            }
+            else
+            {
+                m_nodeState = NodeStates.FAILURE;
+                return m_nodeState;
+            }
+        }
+    }
+}
