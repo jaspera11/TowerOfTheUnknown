436736254d5fd84ad62a8721d3558d2c102c5105
jaspera11 <33080050+jaspera11@users.noreply.github.com>
2019-03-20 18:11:02 -0400

    Add files via upload

--- EnemyPhaseState.cs
+++ EnemyPhaseState.cs
@@ -4,85 +4,112 @@ using UnityEngine;
 using UnityEngine.UI;
 using BT;
 
-public class EnemyPhaseState : MonoBehaviour
+namespace Combat
 {
-    public GameObject SkillsList;
-    public GameObject CombatStateMachine;
-    public GameObject PlayerStats;
-    public GameObject EnemyStats;
-    public Text StatusWindowText;
-    public Text HealthText;
-    public Text StaminaText;
-
-    // Start is called before the first frame update
-    void Start()
+    public class EnemyPhaseState : MonoBehaviour
     {
-        StatusWindowText.text = "The Enemy is Contemplating...";
-        HealthText.text = "";
-        StaminaText.text = "";
-    }
+        public GameObject SkillsList;
+        public GameObject CombatStateMachine;
+        public GameObject PlayerStats;
+        public GameObject EnemyStats;
+        public Text StatusWindowText;
+        public Text HealthText;
+        public Text StaminaText;
 
-    public bool playerBuffedDefense()
-    {
-        if(PlayerStats.GetComponent<UnitStats>().defense.defense > 10)
+        // Start is called before the first frame update
+        void Start()
         {
-            return true;
+            StatusWindowText.text = "The Enemy is Contemplating...";
+            HealthText.text = "";
+            StaminaText.text = "";
         }
-        else
+
+        public bool playerBuffedDefense()
         {
-            return false;
+            //Implement base defense stat?
+            if (PlayerStats.GetComponent<UnitStats>().defense > 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
-    }
 
-    public void Attack()
-    {
-        CombatStateMachine.GetComponent<CombatStateM>().movePower = 5;
-        StatusWindowText.text = "The enemy strikes";
-    }
-
-    public void debuffDefense()
-    {
-        CombatStateMachine.GetComponent<CombatStateM>().movePower = 0;
-        PlayerStats.GetComponent<UnitStats>().defense = PlayerStats.GetComponent<UnitStats>().defense / 2;
-        StatusWindowText.text = "The enemy screeched, lowering your defense";
-    }
-
-    public void chooseOption()
-    {
-        List<Node> level2 = new List<Node>();
-        level2.Add(new Call(Attack));
-        List<Node> level3 = new List<Node>();
-        level3.Add(new Call(debuffDefense));
-        level2.Add(new If(playerBuffedDefense,level3));
-        Selector Root = new Selector(level2);
-        /*Current implementation: First check if player buffed defense.  If yes, debuff defense defense.  If no, attack
-        */
-
-        //if (PlayerStats.GetComponent<UnitStats>().defense > 10)
-        //{
-        //    CombatStateMachine.GetComponent<CombatStateM>().movePower = 0;
-        //    PlayerStats.GetComponent<UnitStats>().defense = PlayerStats.GetComponent<UnitStats>().defense / 2;
-        //    StatusWindowText.text = "The enemy screeched, lowering your defense";
-        //}
-        //else
-        //{
-        //    CombatStateMachine.GetComponent<CombatStateM>().movePower = 5;
-        //    StatusWindowText.text = "The enemy strikes";
-            
-        //}
-
-        if(Root.Evaluate() == Node.NodeStates.SUCCESS)
+        public void basicAttack()
         {
-            CombatStateMachine.GetComponent<CombatStateM>().currState = CombatStateM.CombatState.EnemyCombat;
-        } 
+            CombatStateMachine.GetComponent<CombatStateM>().movePower = 5;
+            StatusWindowText.text = "The enemy strikes";
+        }
 
-    }
+        public void debuffDefense()
+        {
+            CombatStateMachine.GetComponent<CombatStateM>().movePower = 0;
+            PlayerStats.GetComponent<UnitStats>().defense /= 2;
+            StatusWindowText.text = "The enemy screeched, lowering your defense";
+            //EnemyStats.GetComponent<UnitStats>().stamina -= 5;
+        }
 
-    
+        public void shoot()
+        {
+            CombatStateMachine.GetComponent<CombatStateM>().movePower = 5;
+            StatusWindowText.text = "The enemy fired their weapon!";
+            EnemyStats.GetComponent<UnitStats>().stamina -= 10;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public bool enoughSP()
+        {
+            if (EnemyStats.GetComponent<UnitStats>().stamina > 10)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public void chooseOption()
+        {
+            int[] rootWeights = { 40, 40, 20 };
+            List<Node> level2 = new List<Node>();
+            List<Node> level3_1 = new List<Node>();
+            List<Node> level3_2 = new List<Node>();
+            level2.Add(new Call(basicAttack));
+            level3_1.Add(new Call(debuffDefense));
+            level3_2.Add(new Call(shoot));
+            level2.Add(new If(playerBuffedDefense, level3_1));
+            level2.Add(new If(enoughSP, level3_2));
+            RandomSelector Root = new RandomSelector(3, level2, rootWeights);
+            /*Current implementation: First check if player buffed defense.  If yes, debuff defense defense.  If no, attack
+            */
+
+            //if (PlayerStats.GetComponent<UnitStats>().defense > 10)
+            //{
+            //    CombatStateMachine.GetComponent<CombatStateM>().movePower = 0;
+            //    PlayerStats.GetComponent<UnitStats>().defense = PlayerStats.GetComponent<UnitStats>().defense / 2;
+            //    StatusWindowText.text = "The enemy screeched, lowering your defense";
+            //}
+            //else
+            //{
+            //    CombatStateMachine.GetComponent<CombatStateM>().movePower = 5;
+            //    StatusWindowText.text = "The enemy strikes";
+
+            //}
+
+            if (Root.Evaluate() == Node.NodeStates.SUCCESS)
+            {
+                CombatStateMachine.GetComponent<CombatStateM>().currState = CombatStateM.CombatState.EnemyCombat;
+            }
+
+        }
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
