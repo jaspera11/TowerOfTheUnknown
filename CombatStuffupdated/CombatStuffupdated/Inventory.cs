7c53394daa972c96d82adc3742771cf95d3f73a2
YoungManS <seanallenryoung@ufl.edu>
2019-04-05 14:17:24 -0400

    Older versions

---
+++ CombatStuffupdated/CombatStuffupdated/Inventory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int heal = 10;
+    public int energy = 10;
+    public int atkbst = 1;
+    public int defbst = 1;
+    public int spdbst = 1;
+    public int critbst = 1;
+}
