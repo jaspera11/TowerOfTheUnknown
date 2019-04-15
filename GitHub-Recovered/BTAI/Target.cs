b345efd07edf9a7eaa7afbc6f0928951c61c21df
jaspera11 <33080050+jaspera11@users.noreply.github.com>
2019-04-15 13:50:22 -0400

    Add files via upload

---
+++ GitHub-Recovered/BTAI/Target.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+//namespace BT
+//{
+public class Target : Node
+{
+    private Action<UnitStats> m_f;
+    private UnitStats m_target;
+
+    public Target(Action<UnitStats> f, UnitStats target)
+    {
+        m_f = f;
+        m_target = target;
+    }
+
+    public override NodeStates Evaluate()
+    {
+        m_f(m_target);
+
+        return NodeStates.SUCCESS;
+    }
+
+}
+//}
