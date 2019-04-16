66dbaba44c8d3e27093a14a0e46e1b114d48a6e9
Alexander Jasper <jaspera11@ufl.com>
2019-04-15 23:30:07 -0400

    Updated scripts

--- Assets/Scripts/LevelChange.cs
+++ Assets/Scripts/LevelChange.cs
@@ -1,34 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine;
 
+// Attach to object that player exits through
 public class LevelChange : MonoBehaviour
 {
     public int sceneIndex;
-    public int requiredSwitches;
+    public bool quitOnChange = false;
+    public string condition;
+
+    // Depends on condition
+    public int requiredSwitches = 1;
     public int activatedSwitches = 0;
 
-    public void incrementActiveSwitches()
+    public GameObject enemyPrefab;
+    private string enemyName;
+    public int requiredRiddles = 2;
+    public int solvedRiddles = 0;
+
+    void Start()
     {
-        activatedSwitches++;
+        enemyName = enemyPrefab.name;
     }
 
-    public void decrementActiveSwitches()
+    // Also depends on condition
+    private bool ChangeCondition(string condition)
     {
-        activatedSwitches--;
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        //other.name should equal the root of your Player object
-        if (other.CompareTag("Player") && activatedSwitches >= requiredSwitches)
+        switch (condition)
         {
-            //The scene number to load (in File->Build Settings)
-            SceneManager.LoadScene(sceneIndex);
-
-            //Debug.Log("I QUIT!");
-            //Application.Quit();
+            case "switches":
+                return activatedSwitches >= requiredSwitches;
+            case "riddles":
+                return PlayerPrefab.gameData.isDeadEnemy.ContainsKey(enemyName) && solvedRiddles >= requiredRiddles;
+            default:
+                return true;
         }
     }
+
+    // When player walks through exit, quit or change to next level
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && ChangeCondition(condition))
+        {
+            if (quitOnChange)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                // Destroy all enemy objects (prevents redundancy)
+                UnitStats.DestroyByTag("EnemyData");
+                SceneManager.LoadScene(sceneIndex);
+            }
+        }
+    }
+
+    // Increment/decrement activated switches
+    public void IncrementActiveSwitches() { activatedSwitches++; }
+    public void DecrementActiveSwitches() { activatedSwitches--; }
+
+    public void IncrementSolvedRiddles() { solvedRiddles++; }
 }
